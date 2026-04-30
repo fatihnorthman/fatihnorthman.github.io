20f82f0d3308bfaaefd81fc3d5d775eff3016c41
Muhammet Fatih Şahan <sahanfatih2798@gmail.com>
2026-04-30 15:36:30 +0300

    Ultra-subtle 3D tilt with deep premium shadows

--- static/js/interactions.js
+++ static/js/interactions.js
@@ -33,9 +33,9 @@ function init3DTilt() {
             const centerX = rect.width / 2;
             const centerY = rect.height / 2;
             
-            // Böleni 10'dan 30'a çıkararak eğimi 3 kat daha ince yaptım
-            const rotateX = (y - centerY) / 30; 
-            const rotateY = (centerX - x) / 30;
+            // Böleni 30'dan 60'a çıkararak hareketi iyice yumuşattım
+            const rotateX = (y - centerY) / 60; 
+            const rotateY = (centerX - x) / 60;
             
             card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.01)`;
         });
