aaae59154e94e4e0ff06105733160b071700dbb1
Muhammet Fatih Şahan <sahanfatih2798@gmail.com>
2026-04-30 15:31:13 +0300

    Enhance scroll reveal with pop (scale) effect and better sensitivity

--- static/js/interactions.js
+++ static/js/interactions.js
@@ -50,7 +50,10 @@ async function initInteractions() {
 // --- SCROLL REVEAL ENGINE ---
 function initScrollReveal() {
     const posts = document.querySelectorAll('.post.on-list');
-    const observerOptions = { threshold: 0.1, rootMargin: '0px 0px -50px 0px' };
+    const observerOptions = { 
+        threshold: 0.05, // Çok daha hassas: %5 göründüğünde tetikle
+        rootMargin: '0px 0px -20px 0px' 
+    };
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
